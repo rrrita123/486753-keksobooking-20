472ca96ce8f2090fce0f0c922f813d2c98c5f98d
Margo <rrrita123@mail.ru>
2020-07-11 16:05:19 +0400

    Исправления замечаний работа с сетью2

--- js/messages.js
+++ js/messages.js
@@ -11,7 +11,6 @@ window.messages = (function () {
     cloneError.querySelector('.error__message').innerHTML = 'Ошибка загрузки объявления' + '<br>' + message;
 
     mainElement.appendChild(cloneError);
-    document.querySelector('.error').classList.add('message');
 
     messageAddEvent();
   };
@@ -22,7 +21,6 @@ window.messages = (function () {
 
     var cloneSuccess = successTemplateElement.cloneNode(true);
     mainElement.appendChild(cloneSuccess);
-    document.querySelector('.success').classList.add('message');
 
     messageAddEvent();
   };
@@ -34,19 +32,10 @@ window.messages = (function () {
     }
   };
 
-  var messageElement;
-
   // Функция добавляет обработчик событий
   var messageAddEvent = function () {
-    messageElement = document.querySelector('.message');
     document.addEventListener('keydown', onMessageEscClose);
-    messageElement.addEventListener('click', onMessageClose);
-  };
-
-  // Функция удаляет обработчик событий
-  var messageRemoveEvent = function () {
-    document.removeEventListener('keydown', onMessageEscClose);
-    messageElement.removeEventListener('click', onMessageClose);
+    document.querySelector('.message').addEventListener('click', onMessageClose);
   };
 
   // Обработчик закрытия сообщения на ESC
@@ -56,8 +45,10 @@ window.messages = (function () {
 
   // Функция закрытия сообщения
   var closeMessage = function () {
+    var messageElement = document.querySelector('.message');
+    document.removeEventListener('keydown', onMessageEscClose);
+    messageElement.removeEventListener('click', onMessageClose);
     messageElement.remove();
-    messageRemoveEvent();
   };
 
   return {
